056ff72256827dd432b0ccefe2f02c874509629a
darenlarson <larson.daren@gmail.com>
2019-01-28 15:12:43 -0600

    added some test data to enhancers.js

---
+++ enhancers.js
@@ -0,0 +1,18 @@
+// Initial Data
+const item = [
+    {
+        originalName = 'Lambda Shield',
+        name: 'Lambda Shield',
+        type: 'armor',
+        durability: 100,
+        enhancement: 0,
+    },
+    {
+        originalName = 'Elf Sword',
+        name: 'Elf Sword Shield',
+        type: 'weapon',
+        durability: 100,
+        enhancement: 0,
+    },
+];
+
