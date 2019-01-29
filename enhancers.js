9523a1b750efc64ae418a54d4ce998fb818a775c
darenlarson <larson.daren@gmail.com>
2019-01-28 20:41:01 -0600

    Refactored sucess method. Added fail and repair method. Added tests for all of them

--- enhancers.js
+++ enhancers.js
@@ -1,70 +1,151 @@
 // const items = require('./data.js');
 
-const items = [
-    {
-        originalName: 'Lambda Shield',
-        name: 'Lambda Shield',
-        type: 'armor',
-        durability: 100,
-        enhancement: 0,
-    },
-    {
-        originalName: 'Elf Sword',
-        name: 'Elf Sword',
-        type: 'weapon',
-        durability: 100,
-        enhancement: 0,
-    },
-]
+const item = {
+    originalName: 'Lambda Shield',
+    name: 'Lambda Shield',
+    type: 'armor',
+    durability: 100,
+    enhancement: 0,
+    enhancementLevel: 0,
+}
 
 module.exports = {
     success,
+    fail,
+    repair,
 }
 
 function success(item) {
-    // Find the string that was passed in the array of items
-    const selectedItem = items.find(i =>  i.name === item );
-
-    // ensure a valid item was entered
-    if (selectedItem === undefined) throw new Error('Not a valid item')
-
-    let newDurability;
-    let newEnhancement;
-    let newName;
-
-    // New durability
-    newDurability = selectedItem.durability;
-
-    // New enhancement level
-    if (selectedItem.enhancement < 15) {
-        newEnhancement = selectedItem.enhancement + 1;
-    } else if (selectedItem.enhancement === 15) {
-        newEnhancement = 'PRI';
-    } else if (selectedItem.enhancement === 16) {
-        newEnhancement = 'DUO';
-    } else if (selectedItem.enhancement === 17) {
-        newEnhancement = 'TRI';
-    } else if (selectedItem.enhancement === 18) {
-        newEnhancement = 'TET';
-    } else if (selectedItem.enhancement === 19) {
-        newEnhancement = 'PEN';
-    } else if (selectedItem.enhancement === 'PEN') {
-        newEnhancement = 'PEN'
+    // Throw an error if an object is not passed
+    if(typeof item !== 'object') {
+        throw new Error('Expecting an object');
     }
 
-    // New name
-    if (newEnhancement > 0) {
-        newName = `[+${newEnhancement}] ${selectedItem.name}`;
+    // selectedItem is the item that will be changed.
+    let selectedItem = item;
+
+    // New enhancementLevel
+    let newEnhancementLevel;
+
+    if (selectedItem.enhancementLevel === 20) {
+        // 20 is the max enhancement level
+        newEnhancementLevel = 20;
+    } else {
+        newEnhancementLevel = selectedItem.enhancementLevel + 1;
+    }
+
+    // New enhancement. This is equal to the enhancementLevel, unless the enhancementLevel is 16 or above. Then it needs to be the Arabic numerals.
+    let newEnhancement;
+
+    if (newEnhancementLevel <= 15) {
+        newEnhancement = `+${newEnhancementLevel}`;
+    } else if (newEnhancementLevel === 16) {
+        newEnhancement = 'PRI';
+    } else if (newEnhancementLevel === 17) {
+        newEnhancement = 'DUO';
+    } else if (newEnhancementLevel === 18) {
+        newEnhancement = 'TRI';
+    } else if (newEnhancementLevel === 19) {
+        newEnhancement = 'TET';
+    } else if (newEnhancementLevel === 20) {
+        newEnhancement = 'PEN';
+    };
+
+    // Set the updated name.
+    let newName;
+
+    if (newEnhancementLevel > 0) {
+        newName = `[${newEnhancement}] ${selectedItem.originalName}`;
     } else {
         newName = selectedItem.name;
     }
 
-    let updatedItem = {
+    // Create the new object that will be returned.
+    selectedItem = {
         ...selectedItem,
         name: newName,
         enhancement: newEnhancement,
+        enhancementLevel: newEnhancementLevel,
+    }
+
+    return selectedItem;
+};
+
+function fail(item) {
+    // Throw an error if an object is not passed
+    if(typeof item !== 'object') {
+        throw new Error('Expecting an object');
+    }
+
+    // selectedItem is the item that will be changed.
+    let selectedItem = item;
+
+    // New enhancementLevel
+    let newEnhancementLevel;
+
+    if (selectedItem.enhancementLevel > 16) {
+        newEnhancementLevel = selectedItem.enhancementLevel -1;
+    };
+    
+    // New enhancement. This is equal to the enhancementLevel, unless the enhancementLevel is 16 or above. Then it needs to be the Arabic numerals.
+    let newEnhancement;
+
+    if (newEnhancementLevel <= 15) {
+        newEnhancement = newEnhancementLevel;
+    } else if (newEnhancementLevel === 16) {
+        newEnhancement = 'PRI';
+    } else if (newEnhancementLevel === 17) {
+        newEnhancement = 'DUO';
+    } else if (newEnhancementLevel === 18) {
+        newEnhancement = 'TRI';
+    } else if (newEnhancementLevel === 19) {
+        newEnhancement = 'TET';
+    } else if (newEnhancementLevel === 20) {
+        newEnhancement = 'PEN';
+    };
+
+    // Set the updated name.
+    if (newEnhancementLevel > 0) {
+        newName = `[${newEnhancement}] ${selectedItem.originalName}`;
+    } else {
+        newName = selectedItem.name;
+    }
+
+    // update the durability level
+    let newDurability;
+
+    if (newEnhancementLevel <=14) {
+        newDurability = selectedItem.durability - 5;
+    } else {
+        newDurability = selectedItem.durability - 10;
+    }
+
+    // Create the new object that will be returned.
+    selectedItem = {
+        ...selectedItem,
+        name: newName,
+        enhancement: newEnhancement,
+        enhancementLevel: newEnhancementLevel,
         durability: newDurability
     }
 
-    return updatedItem;
+    return selectedItem;
+};
+
+function repair(item) {
+    // Throw an error if an object is not passed
+    if(typeof item !== 'object') {
+        throw new Error('Expecting an object');
+    }
+
+    // selectedItem is the item that will be changed.
+    let selectedItem = item;
+
+
+    selectedItem = {
+        ...selectedItem,
+        durability: 100
+    }
+
+    return selectedItem;
 };
--- enhancers.spec.js
+++ enhancers.spec.js
@@ -2,29 +2,24 @@ const enhancer = require('./enhancers.js');
 
 describe('enhancer library', () => {
     describe('success() method', () => {
-        it('should throw an error if the item entered is not in the items array', () => {
-            expect(() => {
-                enhancer.success('Elf Swords');
-            }).toThrow();
-            expect(() => {
-                enhancer.success(2452);
-            }).toThrow();
-            expect(() => {
-                enhancer.success(undefined);
-            }).toThrow();
-        });
-
-        test('enhancing success increases enhancing level', () => {
-
+        test('enhancing success increases enhancing level and updates name', () => {
             // arrange / setup
-            const input = 'Lambda Shield'
+            const input = {
+                originalName: 'Lambda Shield',
+                name: 'Lambda Shield',
+                type: 'armor',
+                durability: 100,
+                enhancement: 0,
+                enhancementLevel: 0,
+            }
 
             const expected = {
                 originalName: 'Lambda Shield',
                 name: '[+1] Lambda Shield',
                 type: 'armor',
                 durability: 100,
-                enhancement: 1,
+                enhancement: '+1',
+                enhancementLevel: 1,
             };
 
             // act - execute the SUT (System Under Test)
@@ -34,6 +29,64 @@ describe('enhancer library', () => {
             expect(actual).toEqual(expected);
         });
 
+        it('should throw an error if an object is not passed in as an arguemtn', () => {
+            expect(() => {
+                enhancer.success('this is a string');
+            }).toThrow();
+        });
     });
-});
+    describe('fail() method', () => {
+        test('enhancing fail decreases enhancementLevel by 1 if it is currently > 16', () => {
+            // arrange / setup
+            const input = {
+                originalName: 'Lambda Shield',
+                name: '[PEN] Lambda Shield',
+                type: 'armor',
+                durability: 100,
+                enhancement: 'PEN',
+                enhancementLevel: 20,
+            };
+            const expected = {
+                originalName: 'Lambda Shield',
+                name: '[TET] Lambda Shield',
+                type: 'armor',
+                durability: 90,
+                enhancement: 'TET',
+                enhancementLevel: 19,
+            };
 
+            // act - execute the SUT (System Under Test)
+            const actual = enhancer.fail(input);
+
+            // assert
+            expect(actual).toEqual(expected);
+        })
+    })
+    describe('repair() method', () => {
+        it('should increase the durability to 100 and return the rest of the object unchanged', () => {
+            // arrange / setup
+            const input = {
+                originalName: 'Lambda Shield',
+                name: '[PEN] Lambda Shield',
+                type: 'armor',
+                durability: 50,
+                enhancement: 'PEN',
+                enhancementLevel: 20,
+            };
+            const expected = {
+                originalName: 'Lambda Shield',
+                name: '[PEN] Lambda Shield',
+                type: 'armor',
+                durability: 100,
+                enhancement: 'PEN',
+                enhancementLevel: 20,
+            };
+
+            // act - execute the SUT (System Under Test)
+            const actual = enhancer.repair(input);
+
+            // assert
+            expect(actual).toEqual(expected);
+        });
+    });
+})
